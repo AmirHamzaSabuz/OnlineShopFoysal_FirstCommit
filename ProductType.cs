5b86d8e7de14741a483fbb139bec0bc18f9a751b
HR <hamza_amir@yahoo.com>
2024-02-02 08:07:41 +0600

    first commit

---
+++ ProductType.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace OnlineShop2.Models
+{
+    public class ProductType
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Product Type")]
+        public string ProductTypeName { get; set; }
+    }
+}
